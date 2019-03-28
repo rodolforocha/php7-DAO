3b87081e17efcf5bd48596dce0e7e42bf39ab50d
Rodolfo Rocha <rodolfo.rocha@ccc.ufcg.edu.br>
2019-03-28 15:54:37 -0300

    Inicio Projeto

---
+++ dbophp.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
+
+$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($results);
+
+
+?>
